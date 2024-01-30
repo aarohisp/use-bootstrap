d83269d701f0f59c4b2796ad237d988bfc72903d
aarohisp <112809842+aarohisp@users.noreply.github.com>
2024-01-30 10:02:30 +0530

    added all cards to one cardscollection component

---
+++ src/components/CardsCollection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CardsCollection() {
+  return (
+    <div classname="row">
+      <card>
+        <card>
+          <card>
+            <card></card>
+          </card>
+        </card>
+      </card>
+    </div>
+  );
+}
